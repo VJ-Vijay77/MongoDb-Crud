d4b896aa2dfb9ea2c9471beefecdb486110c787b
VJ-Vijay77 <vijaydinesh77vj@gmail.com>
2022-09-03 21:28:05 +0530

    Retrieve data done

--- controllers/crud.go
+++ controllers/crud.go
@@ -9,19 +9,56 @@ import (
 	"github.com/VJ-Vijay77/mongodb/errpkg"
 	"github.com/VJ-Vijay77/mongodb/models"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-
+// var D *config.DbVariable
 
 func GetPerson(c *fiber.Ctx) error {
+	k := db.NewDbData()
+	if k.CollectionName == "" || k.DbName == "" {
+		log.Fatalln("couldnt get env data")
+	}
+	 collection,err := db.GetMongodbCollection(k.DbName,k.CollectionName)
+	 if err != nil {
+		return c.Status(500).Send([]byte(err.Error()))
+	}
+
+	var filter bson.M = bson.M{}
+
+	if c.Params("id") != "" {
+		id := c.Params("id")
+		objId,_ := primitive.ObjectIDFromHex(id)
+		filter = bson.M{"_id":objId}
+	}
+
+	var results []bson.M
+	cur,err := collection.Find(context.Background(),filter)
+	errpkg.StatusFiveHundred(c,err)
+	
+	defer cur.Close(context.Background())
+
+	cur.All(context.Background(),&results)
+
+	if results == nil {
+		c.SendStatus(404)
+		return nil
+	}
+
+	json,_ := json.Marshal(results)
+	c.Send(json)
+
+
+
 	return nil
 }
 
 func CreatePerson(c *fiber.Ctx) error {
-	k:= db.NewDbData()
+
+	k := db.NewDbData()
 	if k.CollectionName == "" || k.DbName == "" {
 		log.Fatalln("couldnt get env data")
-	
 	}
 	collection, err := db.GetMongodbCollection(k.DbName, k.CollectionName)
 	if err != nil {
